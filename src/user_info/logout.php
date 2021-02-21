9f407bf208222e1f5be487df8f1b94b46e6db3d5
kimura00001 <kimura.motoyasu@gmail.com>
2021-02-22 00:31:34 +0900

    no message

--- src/user_info/logout.php
+++ src/user_info/logout.php
@@ -10,34 +10,19 @@
         ORM::configure('mysql:host='.Env::get("HOST").';port='.Env::get("PORT").';dbname='.Env::get("DB_NAME"));
         ORM::configure('username', Env::get('USER_ID'));
         ORM::configure('password', Env::get("PASSWORD"));
+        ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
         $table = 'user'; //テーブルの名前
         // $query = ORM::for_table($table)->find_one();
         // var_dump($query->as_array());
         
 
-        $token = NULL;//$_POST['token'];//tokenを取得し変数へ格納
-        var_dump($token);
-        $update = ORM::for_table($table)->where('token', $token) ->find_one();
-        $update->token = "ウンチ";
-                $update->save('token', $token);
+        $token = 'ウンチ';//$_POST['token'];//tokenを取得し変数へ格納
+        $id = 1;
+        $update = ORM::for_table($table)->where('user_id', $id)->find_result_set()
+        ->set('token', 'pasu')
+        ->save();
                 var_dump($update);
 
-        // {
-        //     "result": true
-            
-        // }
-        // {
-        //     "error": [
-        //     {
-        //         "code": エラーコード(int),
-        //         "message": エラーメッセージ(string)
-        //     },
-        //     {
-        //         "code": 同上,
-        //         "message": 同上
-        //     }
-        //     ]
-        // }
         
 
