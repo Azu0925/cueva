fde16fc8714ad6f630ca976aa98258545df98847
kimura00001 <kimura.motoyasu@gmail.com>
2021-02-24 01:10:21 +0900

    レビューお願いします。

--- src/user_info/logout.php
+++ src/user_info/logout.php
@@ -1,42 +1,55 @@
     <?php
     //これをsrc直下にコピーしてファイル名の「.sample」部分を削除して動かしてちょ
 
-        use Cueva\Classes\ {Env, Func};
+    use Cueva\Classes\{Env, Func};
 
-        require_once '../../vendor/j4mie/idiorm/idiorm.php';
-        require '../../vendor/autoload.php';
-
-        
-        ORM::configure('mysql:host='.Env::get("HOST").';port='.Env::get("PORT").';dbname='.Env::get("DB_NAME"));
-        ORM::configure('username', Env::get('USER_ID'));
-        ORM::configure('password', Env::get("PASSWORD"));
-        ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-
-        $table = 'user'; //テーブルの名前
-        $token = 'papalaalala';//$_POST['token'];//tokenを取得し変数へ格納
-
-        $update = ORM::for_table('user')->where('token', $token)->find_one();
-        $update->token = NULL;
-        $update->save();
-        
-        var_dump($update);
+    require_once '../../vendor/j4mie/idiorm/idiorm.php';
+    require '../../vendor/autoload.php';
 
 
-/*
-        $token = 'ウンチ';//$_POST['token'];//tokenを取得し変数へ格納
-        try{
-        $update = ORM::for_table($table)->where('token', 'ウンチ')->find_one();
-        $update->token = 'パスワード';
-        $update = save();
-        var_dump($update);
-    }catch (PDOException $e){
-        echo "エラー発生：".$e->getMessage();
-      }catch (Exception $e) {
-        echo "エラー発生：".$e->getMessage();
-      }
-                var_dump($update);*/
+    ORM::configure('mysql:host=' . Env::get("HOST") . ';port=' . Env::get("PORT") . ';dbname=' . Env::get("DB_NAME"));
+    ORM::configure('username', Env::get('USER_ID'));
+    ORM::configure('password', Env::get("PASSWORD"));
+    ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    $table = 'user'; //テーブルの名前
 
+    //tokenカラムをNULLにアップデート
+    if (isset($_POST['token'])) {
+      $token = $_POST['token']; //tokenを取得し変数へ格納
+      $update = ORM::for_table($table)->where('token', $token)->find_one();
+      $update->token = NULL;
+      $update->save();
+    
+    if (($update->save())) {
+      $result = array(
+        "result" => array(
+          array(
+            "result" => true
+          )
+        )
+      );
+      echo json_encode($result);
+      exit;
+    }else{
+      $error = array(
+        "error" => array(
+          array(
+            "code" => "452",
+            "message" => "Update error for database"
+          )
+        )
+      );
+      echo json_encode($error);
+      exit;
+    }
+  }
+  $error = array(
+    "error" => array(
+      array(
+        "code" => "404",
+        "message" => "Not Found"
+      )
+    )
+  );
 
-
-        
-
+    echo json_encode($error);
