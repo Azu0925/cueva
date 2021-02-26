9cfc672092b8062c95ecef6323b0dd9b357ed2a0
kimura00001 <kimura.motoyasu@gmail.com>
2021-02-27 02:33:47 +0900

    一応全部書きましたが、まだ自分でデバックしてないです。

--- src/card_info/card_update.php
+++ src/card_info/card_update.php
@@ -6,14 +6,81 @@
     require_once '../../vendor/j4mie/idiorm/idiorm.php';
     require '../../vendor/autoload.php';
 
-
+    date_default_timezone_set('Asia/Tokyo'); //タイムゾーン設定
     ORM::configure('mysql:host=' . Env::get("HOST") . ';port=' . Env::get("PORT") . ';dbname=' . Env::get("DB_NAME"));
     ORM::configure('username', Env::get('USER_ID'));
     ORM::configure('password', Env::get("PASSWORD"));
     ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
     $table = 'v_map_delete'; //テーブルの名前
 
-    //$testToken = 'test';
+
+    $card_name = $_POST['card_name']; //card_name 作成するカードの名前
+    if ((isset($card_name) == false) && ($card_name == NULL)) { //バリデーションチェック
+      $error = array(
+        "error" => array(
+          array(
+            "code" => "400",
+            "message" => "Bad Request"
+          )
+        )
+      );
+      echo json_encode($error);
+      exit;
+    }
+    $card_description = $_POST['card_description']; //card_description 作成するカードの詳細
+
+    $card_x = $_POST['card_x']; //card_x カードのx座標
+    if ((is_int($card_x) == false) && ($card_x == NULL)) {  //バリデーションチェック
+      $error = array(
+        "error" => array(
+          array(
+            "code" => "400",
+            "message" => "Bad Request"
+          )
+        )
+      );
+      echo json_encode($error);
+      exit;
+    }
+    $card_y = $_POST['card_y']; //card_y カードのy座標
+    if ((is_int($card_y) == false) && ($card_y == NULL)) {  //バリデーションチェック
+      $error = array(
+        "error" => array(
+          array(
+            "code" => "400",
+            "message" => "Bad Request"
+          )
+        )
+      );
+      echo json_encode($error);
+      exit;
+    }
+    $card_width = $_POST['card_width']; //card_width カードの横幅
+    if ((is_int($card_width) == false) && ($card_width == NULL)) {  //バリデーションチェック
+      $error = array(
+        "error" => array(
+          array(
+            "code" => "400",
+            "message" => "Bad Request"
+          )
+        )
+      );
+      echo json_encode($error);
+      exit;
+    }
+    $card_height = $_POST['card_height']; //card_height カードの縦の長さ
+    if ((is_int($card_height) == false) && ($card_height == NULL)) {  //バリデーションチェック
+      $error = array(
+        "error" => array(
+          array(
+            "code" => "400",
+            "message" => "Bad Request"
+          )
+        )
+      );
+      echo json_encode($error);
+      exit;
+    }
 
     //tokenとmap_idの検索
     if (isset($_POST['token']) && isset($_POST['map_id'])) {
@@ -25,10 +92,21 @@
           'm_id' => $map_id
         ))
         ->find_many();
-      if ($select != false) { //map削除処理
-        $delete = ORM::for_table("map")->where('id', $map_id)->find_one();
-        if ($delete != false) {
-          $delete->delete();
+      if ($select != false) { //card更新ユーザー名の取得
+        $record = ORM::for_table('user')->where('token', $token)->find_one();
+        $update_user = $record->name;
+        $card_id = $_POST['card_id']; //card_id 更新するカードのid
+        $update = ORM::for_table("card")->where('id', $card_id)->find_one(); //card更新処理１
+        if ($update != false) { //card更新処理２(更新内容の挿入)
+          $update->card_name = $card_name;
+          $update->card_description = $card_description;
+          $update->update_date = date("Y/m/d H:i:s");
+          $update->update_user = $update_user;
+          $update->card_x = $card_x;
+          $update->card_y = $card_y;
+          $update->card_width = $card_width;
+          $update->card_height = $card_height;
+          $update->save(); //更新
           $result = array(
             "result" => array(
               array(
@@ -54,8 +132,8 @@
         $error = array(
           "error" => array(
             array(
-              "code" => "400",
-              "message" => "Bad Request"
+              "code" => "403",
+              "message" => "Forbidden"
             )
           )
         );
@@ -67,8 +145,8 @@
     $error = array(
       "error" => array(
         array(
-          "code" => "404",
-          "message" => "Not Found"
+          "code" => "453",
+          "message" => "Paramter is null"
         )
       )
     );
