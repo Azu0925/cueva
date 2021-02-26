2e7be8a857b9e502f76e426c1d1b381fc104babd
宝田雄希 <takarayuki0717@gmail.com>
2021-02-26 15:16:29 +0900

    no message

--- src/team_info/register.php
+++ src/team_info/register.php
@@ -12,17 +12,16 @@
 
 
     //入力値の受け取り
-    $team_name = "kokoko";//$_POST['team_name'];
-    $team_description = "kokoko";//$_POST['team_description'];
-    //$team_create = $_POST['team_create'];
-    $team_host = "kokoko";//$_POST['team_host'];
+    $team_name = $_POST['team_name'];
+    $team_description = $_POST['team_description'];
+    $team_host = $_POST['team_host'];
     
     //table指定
     $user_table = 'user';
     $team_table = 'team';
 
     //tokenの取得
-    $token = $_COOKIE['token'];
+    $token = $_POST['token'];
 
     //登録されているユーザー情報取得
     $user_list = ORM::for_table($user_table)->where('token', $token)->find_one();
@@ -33,22 +32,22 @@
     }
     // var_dump($list);
     
-    //tokenの照合
-    if($token !== $user_list['token']){
-        //エラー内容
-        //jsonでエラーメッセージの返却
-        $err = array('error' =>
-        array( 
-        array('code' => '401','message' => 'Unauthorized')),
-    );
-        echo json_encode($err);
-}
+//     //tokenの照合
+//     if($token !== $user_list['token']){
+//         //エラー内容
+//         //jsonでエラーメッセージの返却
+//         $err = array('error' =>
+//         array( 
+//         array('code' => '401','message' => 'Unauthorized')),
+//     );
+//         echo json_encode($err);
+//         exit;
+// }
 
     //チーム新規作成
     $new_team = ORM::for_table($team_table)->create();
     $new_team->team_name = $team_name;
     $new_team->team_description = $team_description;
-    //$new_team->team_create = $team_create;
     $new_team->team_host = $team_host;
     $new_team->save();
 
