fed069cb9611e33e5811ea99eeef624b17188e31
sinano-roll <harada.kazuki01@gmail.com>
2021-02-28 00:09:37 +0900

    カード生成　カードの名前と説明文以外初期値入ると思って空けてます

--- card_info/create_card.php
+++ card_info/create_card.php
@@ -14,22 +14,26 @@ use Cueva\Classes\ {Env, Func};
 $table = 'card';
 //カード作成者がチームに所属しているかどうか
 
-//バリデーションチェック
-if ((empty($_POST['card_name']))) {
-    $error = array(
-        "error" => array(
-            array(
-                "code" => "451",
-                "message" => "Validation error for 'card_name'"
-            )
-        )
-    );
-    echo json_encode($error);
-    exit;
+$card_name = $_POST['card_name']; //card_name 作成するカードの名前
+if ((isset($card_name) == false) && ($card_name == NULL)) { //バリデーションチェック
+  $error = array(
+    "error" => array(
+      array(
+        "code" => "400",
+        "message" => "Bad Request"
+      )
+    )
+  );
+  echo json_encode($error);
+  exit;
 }
-//入力値の受け取り
-$card_name = $_POST['card_name'];
-$card_discription = $_POST['discription'];
+$card_discription = $_POST['discription'];//カードの説明文
+$update = '初期値';
+$card_x = '初期値';
+$card_y = '初期値';
+$card_width	= '初期値';
+$card_height = '初期値';
+
 //名前の文字数制限
 $max = 30;
 $min = 1;
@@ -48,31 +52,64 @@ if($max < $name_len || $name_len < $min){
 }
 //カードの説明の文字数制限
 
-//カード情報の登録
-$person = ORM::for_table($table)->create();
-$person->card_name = $card_name;
-$person->card_discription = $card_discription;
-$person->save();
-//insert失敗の処理   
-if(!$person->save()){
-    $error = array(
+//tokenとmap_idの検索
+if (isset($_POST['token']) && isset($_POST['map_id'])) {
+    $token = $_POST['token'];  //tokenを取得し変数へ格納
+    $map_id = $_POST['map_id']; //map_idを取得し変数へ格納
+    $select = ORM::for_table('v_map_delete')
+        ->where(array(
+        'token' => $token,
+        'm_id' => $map_id
+        ))
+        ->find_many();
+    if ($select != false) { //card作成ユーザー名の取得
+        $record = ORM::for_table('user')->where('token', $token)->find_one();
+        $update_user = $record->name;
+    if ($update_user != false) { //card更新処理２(更新内容の挿入)
+        //カード情報の登録  
+        //名前と説明以外は初期値が入っている前提
+        $person = ORM::for_table($table)->create();
+        $person->card_name = $card_name;
+        $person->card_discription = $card_discription;
+        $person->update =$update;
+        $person->card_x =$card_x;
+        $person->card_y =$card_y;
+        $person->card_width =$card_width;
+        $person->card_height =$card_height;
+        $person->save();
+        $result = array(
+            "result" => array(
+            array(
+                "result" => true
+            )
+            )
+        );
+        echo json_encode($result);
+        exit;
+        } 
+    } else { //tokenとmap_idに関連性がなかった場合(チームメンバー以外の削除リクエスト)
+        $error = array(
         "error" => array(
             array(
-                "code" => "452",
-                "message" => "Insert error for database"
+            "code" => "403",
+            "message" => "Forbidden"
             )
         )
-    );
-    echo json_encode($error);
-    exit;
-}
-
-//jsonの返却
-$response = array(
-    "result" => array(
-        "token" => $token
+        );
+        echo json_encode($error);
+        exit;
+    }
+    }
+    //tokenとmap_idが取得できなかった場合
+    $error = array(
+    "error" => array(
+        array(
+        "code" => "453",
+        "message" => "Paramter is null"
+        )
     )
-);
-echo json_encode($response);
+    );
+
+    echo json_encode($error);
 ?>
 
