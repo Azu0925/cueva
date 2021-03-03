08a7e3a864ac7556f07d64bbfe6dfd264977ea43
kimura00001 <kimura.motoyasu@gmail.com>
2021-03-03 17:14:43 +0900

    むらｐ指摘箇所修正しました。

--- src/map_info/create_map.php
+++ src/map_info/create_map.php
@@ -13,79 +13,89 @@
     ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
     $table = 'v_map_delete'; //テーブルの名前
 
-    if (isset($_POST['map_name'])) {
-      # code...
 
-      $map_name = $_POST['map_name']; //card_name 作成するカードの名前
-      if ((isset($card_name) == false) && ($card_name == NULL)) { //バリデーションチェック
-        $error = array(
-          "error" => array(
-            array(
-              "code" => "400",
-              "message" => "Bad Request"
-            )
-          )
-        );
-        echo json_encode($error, JSON_UNESCAPED_UNICODE);
-        exit;
-      }
-      $map_description = $_POST['map_description']; //card_description 作成するカードの詳細
+    $map_description = $_POST['map_description'];
+    $map_name = $_POST['map_name']; 
+    $parameter_top = $_POST['parameter_top'];
+    $parameter_under = $_POST['parameter_under'];
+    $parameter_left = $_POST['parameter_left'];
+    $parameter_right = $_POST['parameter_right'];
 
-      //tokenとteam_idの検索
-      if (isset($_POST['token']) && isset($_POST['team'])) {
-        $token = $_POST['token'];  //tokenを取得し変数へ格納
-        $team_id = $_POST['team_id']; //map_idを取得し変数へ格納
-        $select = ORM::for_table('v_map_create')
-          ->where(array(
-            'token' => $token,
-            't_id' => $team_id
-          ))
-          ->find_many();
-        if ($select != false) { //card更新ユーザー名の取得
-          $record = ORM::for_table('user')->where('token', $token)->find_one();
-          $create_user = $record->name;
-          $create_map = ORM::for_table('map')->create(); //map新規作成処理1
-          if ($update != false) { //card更新処理２(更新内容の挿入)
-            $create_map->map_name = $card_name;
-            $create_map->map_description = $card_description;
-            $create_map->map_create = date("Y/m/d H:i:s");
-            $create_map->map_host = $create_user;
-            $create_map->save(); //更新
-            $result = array(
-              "result" => array(
-                array(
-                  "result" => true
-                )
-              )
-            );
-            echo json_encode($result, JSON_UNESCAPED_UNICODE);
-            exit;
-          } else { //Dleteエラー処理
+    //tokenとteam_idの検索
+    if (isset($_POST['token']) && isset($_POST['team_id'])) {
+      $token = $_POST['token'];  //tokenを取得し変数へ格納
+      $team_id = $_POST['team_id']; //map_idを取得し変数へ格納
+      $select = ORM::for_table('v_map_create')
+        ->where(array(
+          'token' => $token,
+          't_id' => $team_id
+        ))
+        ->find_many();
+      if ($select != false) { //map更新ユーザー名の取得
+        $create_user = ORM::for_table('user')->where('token', $token)->find_one();
+        $create_user->user_name;
+        $create_map = ORM::for_table('map')->create(); //map新規作成処理1
+        if ($create_map != false) { //card更新処理２(更新内容の挿入)
+          if (!isset($_POST['map_name'])) { //map_name 作成するmapの名前がなかったらfalse
             $error = array(
               "error" => array(
                 array(
-                  "code" => "452",
-                  "message" => "Delete error for database"
+                  "code" => "400",
+                  "message" => "Bad Request"
                 )
               )
             );
             echo json_encode($error, JSON_UNESCAPED_UNICODE);
             exit;
           }
-        } else { //tokenとmap_idに関連性がなかった場合(チームメンバー以外の削除リクエスト)
+          $create_map->map_name = $map_name;
+          $create_map->map_description = $map_description;
+          $create_map->team_id = $team_id;
+          $create_map->map_create = date("Y/m/d H:i:s");
+          $create_map->map_host = $create_user->user_name;
+          $create_map->parameter_top = $parameter_top;
+          $create_map->parameter_under = $parameter_under;
+          $create_map->parameter_left = $parameter_left;
+          $create_map->parameter_right = $parameter_right;
+          $create_map->save(); //更新
+          $id = $create_map->id;
+          $result = array(
+            "result" => array(
+              array(
+                "result" => true,
+                $id
+
+              )
+            )
+          );
+          echo json_encode($result, JSON_UNESCAPED_UNICODE);
+          exit;
+        } else { //Dleteエラー処理
           $error = array(
             "error" => array(
               array(
-                "code" => "403",
-                "message" => "Forbidden"
+                "code" => "452",
+                "message" => "Delete error for database"
               )
             )
           );
           echo json_encode($error, JSON_UNESCAPED_UNICODE);
           exit;
         }
+      } else { //tokenとmap_idに関連性がなかった場合(チームメンバー以外の削除リクエスト)
+        $error = array(
+          "error" => array(
+            array(
+              "code" => "403",
+              "message" => "Forbidden"
+            )
+          )
+        );
+        echo json_encode($error, JSON_UNESCAPED_UNICODE);
+        exit;
       }
     }
+
     //tokenとteam_idが取得できなかった場合
     $error = array(
       "error" => array(
